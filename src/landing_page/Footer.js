18fb381cdc07c014201457d176b5ca5d8ecabd43
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-06-06 14:00:01 +0530

    Developed Home section

--- src/landing_page/Footer.js
+++ src/landing_page/Footer.js
@@ -2,7 +2,137 @@ import React from 'react';
 
 function Footer() {
     return (
-        <h1>Footer</h1>
+        <div className='container-fluid bg-light border-top mt-5'>
+            <div className='container'>
+                <div className='row mt-5'>
+                    <div className='col'>
+                        <img src="media/images/logo.svg" alt="Logo" style={{ width: "50%" }} />
+                        <p className='mt-3 text-muted' style={{ fontSize: "0.8rem" }}>
+                            &copy; 2010 - 2025, Not Zerodha Broking Ltd. <br />All rights reserved.
+                        </p>
+                    </div>
+                    <div className='col '>
+                        <p>Company</p>
+                        <a className='text-muted ' style={{ textDecoration: "none" }} href="">About</a>
+                        <br />
+                        <a className=' text-muted' style={{ textDecoration: "none" }} href="">Products</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Pricing</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Referral programme</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Careers</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Zerodha.tech</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Press & media</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Zerodha cares (CSR)</a>
+                        <br />
+                    </div>
+                    <div className='col'>
+                        <p>Support</p>
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Contact</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Support portal</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Z-Connect blog</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">List of charges</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Downloads & resources</a>
+                        <br />
+                    </div>
+                    <div className='col'>
+                        <p>Account</p>
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Open an account</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">Fund transfer</a>
+                        <br />
+                        <a className='text-muted' style={{ textDecoration: "none" }} href="">60 day challenge</a>
+                        <br />
+                    </div>
+                </div>
+
+                <div className='mt-5 text-muted' style={{ fontSize: "14px" }}>
+                    <p>
+                        Zerodha Broking Ltd.: Member of NSE​ &​ BSE – SEBI Registration no.:
+                        INZ000031633 CDSL: Depository services through Zerodha Securities
+                        Pvt. Ltd. – SEBI Registration no.: IN-DP-100-2015 Commodity Trading
+                        through Zerodha Commodities Pvt. Ltd. MCX: 46025 – SEBI Registration
+                        no.: INZ000038238 Registered Address: Zerodha Broking Ltd.,
+                        #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School,
+                        J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any
+                        complaints pertaining to securities broking please write to
+                        complaints@zerodha.com, for DP related to dp@zerodha.com. Please
+                        ensure you carefully read the Risk Disclosure Document as prescribed
+                        by SEBI | ICF
+                    </p>
+
+                    <p>
+                        Procedure to file a complaint on SEBI SCORES: Register on SCORES
+                        portal. Mandatory details for filing complaints on SCORES: Name,
+                        PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
+                        Communication, Speedy redressal of the grievances
+                    </p>
+
+                    <p>
+                        Investments in securities market are subject to market risks; read
+                        all the related documents carefully before investing.
+                    </p>
+
+                    <p>
+                        "Prevent unauthorised transactions in your account. Update your
+                        mobile numbers/email IDs with your stock brokers. Receive
+                        information of your transactions directly from Exchange on your
+                        mobile/email at the end of the day. Issued in the interest of
+                        investors. KYC is one time exercise while dealing in securities
+                        markets - once KYC is done through a SEBI registered intermediary
+                        (broker, DP, Mutual Fund etc.), you need not undergo the same
+                        process again when you approach another intermediary." Dear
+                        Investor, if you are subscribing to an IPO, there is no need to
+                        issue a cheque. Please write the Bank account number and sign the
+                        IPO application form to authorize your bank to make payment in case
+                        of allotment. In case of non allotment the funds will remain in your
+                        bank account. As a business we don't give stock tips, and have not
+                        authorized anyone to trade on behalf of others. If you find anyone
+                        claiming to be part of Zerodha and offering such services, please
+                        create a ticket here.
+                    </p>
+                </div>
+                <div className='container' style={{ textAlign: "center" }}>
+                    <ul>
+                        <li className='mx-3' style={{ display: "inline", textDecoration: "none" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} rel="nofollow" href="https://nseindia.com">NSE</a>
+                        </li>
+                        <li className='mx-3' style={{ display: "inline", textDecoration: "none" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} rel="nofollow" href="https://www.bseindia.com/">BSE</a>
+                        </li >
+                        <li className='mx-3' style={{ display: "inline", textDecoration: "none" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} rel="nofollow" href="https://www.mcxindia.com/">MCX</a>
+                        </li>
+                        <li className='mx-3' style={{ display: "inline", textDecoration: "none" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} href="https://zerodha.com/terms-and-conditions/">Terms &amp; conditions</a>
+                        </li>
+                        <li className='mx-3' style={{ display: "inline", textDecoration: "none" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} href="https://zerodha.com/policies-and-procedures/">Policies &amp; procedures</a>
+                        </li>
+                        <li className='mx-3' style={{ display: "inline", textDecoration: "none" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} href="https://zerodha.com/privacy-policy/">Privacy policy</a>
+                        </li>
+                        <li className='mx-3' style={{ display: "inline" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} href="https://zerodha.com/disclosure/">Disclosure</a>
+                        </li>
+                        <li className='mx-3' style={{ display: "inline" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} href="https://zerodha.com/tos/investor-charter/">Investor charter</a>
+                        </li>
+                        <li className='mx-3' style={{ display: "inline" }}>
+                            <a className='text-muted' style={{ textDecoration: "none" }} href="https://zerodha.com/investor-attention/">For investor's attention</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 }
 
