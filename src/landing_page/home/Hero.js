c5a0282a2ef196deaef9782024e97767c8798094
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-06-06 16:25:43 +0530

    router setup, page not found and some minor changes

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -6,8 +6,8 @@ function Hero() {
             <div className='row text-center'>
                 <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5' />
                 <h1 className='mt-5'>Invest in everything</h1>
-                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button style={{ width: "20%", margin: "0 auto" }} className='btn btn-primary p-2 fs-5 mb-5'>Sign up for free</button>
+                <p className='mt-2 fs-5'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button style={{ width: "20%", margin: "0 auto" }} className='btn btn-primary p-2 fs-5 mb-5 mt-4'>Sign up for free</button>
             </div>
         </div>
     );
--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Brokerage from './Brokerage';
 import Hero from './Hero';
-import Footer from '../Footer';
-import Navbar from '../Navbar';
+
 
 function PricingPage() {
     return (
         <>
-            <Navbar />
             <Brokerage />
             <Hero />
-            <Footer />
         </>
     );
 }
