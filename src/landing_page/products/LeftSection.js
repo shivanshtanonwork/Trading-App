7d4386d2ad4d3e81cc0f2f8895dcba4e855faca0
Shivansh Tandon <76089733+shivanshtanonwork@users.noreply.github.com>
2025-06-25 21:54:43 +0530

    Delete src directory

--- src/landing_page/products/LeftSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LeftSection() {
-    return (
-        <h1>LeftSection</h1>
-    );
-}
-
-export default LeftSection;
--- src/landing_page/products/RightSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function RightSection() {
-    return (
-        <h1>Right Section</h1>
-    );
-}
-
-export default RightSection;
