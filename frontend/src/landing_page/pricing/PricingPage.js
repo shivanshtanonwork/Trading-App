cd81c8b149e33b74e27c06ea08b801786e788c3c
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-06-25 22:28:00 +0530

    Fix: removed submodule and re-added frontend as regular folder

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Brokerage from './Brokerage';
+import Hero from './Hero';
+
+
+function PricingPage() {
+    return (
+        <>
+            <Brokerage />
+            <Hero />
+        </>
+    );
+}
+
+export default PricingPage;
