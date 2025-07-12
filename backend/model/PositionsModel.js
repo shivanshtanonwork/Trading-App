dfa1de1d6dc4c68bb0321dd08ef948c5a3a41d38
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-07-12 19:58:41 +0530

    Created Orders, Positons  Schema and Model

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,7 @@
+const { model } = require("mongoose ");
+
+const { PositionsSchema } = require("../schemas/PositonsSchema");
+
+const PositionsModel = new model("position", PositionsSchema);
+
+module.exports = { PositionsModel };
