ce5b3b246598dfcc6ebe809d73a55cf55361c8b3
Shivansh Tandon <76089733+shivanshtanonwork@users.noreply.github.com>
2025-08-25 15:07:55 +0530

    Update Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://trading-app-pa5r.onrender.com/allHoldings").then((res) => {
       setAllHoldings(res.data);
     });
   }, []);
